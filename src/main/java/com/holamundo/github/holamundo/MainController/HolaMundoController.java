35499180c523072f8dc51329fb393bb22c0b4c38
SepUC <mati.sepulvedam@duocuc.cl>
2025-05-05 13:43:28 -0400

    Adición Lorem Ipsium via nuevo mapping /olax2. -Matías Sepúlveda.

--- src/main/java/com/holamundo/github/holamundo/MainController/HolaMundoController.java
+++ src/main/java/com/holamundo/github/holamundo/MainController/HolaMundoController.java
@@ -10,4 +10,10 @@ public class HolaMundoController {
     public String holaMundo() {
         return "Hola Mundo";
     }
+
+    @GetMapping("/olax2")
+    public String holaMund2() {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+                " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    }
 }
