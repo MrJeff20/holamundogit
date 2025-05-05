834c243619a18be1e7d7c0dff3bd6b46a15839e8
MrJeff20 <je.carbonell@duocuc.cl>
2025-05-05 13:28:34 -0400

    Hola Mundo en Spring: Print realizado

--- src/main/java/com/holamundo/github/holamundo/MainController/HolaMundoController.java
+++ src/main/java/com/holamundo/github/holamundo/MainController/HolaMundoController.java
@@ -1,4 +1,13 @@
 package com.holamundo.github.holamundo.MainController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
 }
